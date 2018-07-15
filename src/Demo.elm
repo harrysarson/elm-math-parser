3008a662b3bcaae5f034b568f73e2e4ae4999c67
Harry Sarson <harry.sarson@hotmail.co.uk>
2018-07-15 21:25:18 +0100

    initialise state to correct value and lint

--- src/Demo.elm
+++ src/Demo.elm
@@ -3,35 +3,51 @@ module Demo exposing (main)
 import Html exposing (Html, Attribute, beginnerProgram, text, div, input)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput)
+import Task
 import Parser exposing (run)
 import MathematicsParser exposing (expression)
 
 
+initialModel =
+    "4 + 8"
+
+
 main =
-  beginnerProgram { model = "", view = view, update = update }
+    beginnerProgram
+        { model = initialModel
+        , view = view
+        , update = update
+        }
+
 
 
 -- UPDATE
 
-type Msg = NewContent String
+
+type Msg
+    = NewContent String
+
 
 update (NewContent content) oldContent =
-  content
+    content
+
 
 
 -- VIEW
 
+
 view content =
-  div []
-    [ input [ defaultValue "4 + 8", onInput NewContent, myStyle ] []
-    , div [ myStyle ] [ text (toString <| run expression content) ]
-    ]
+    div []
+        [ input [ defaultValue initialModel, onInput NewContent, myStyle ] []
+        , div [ myStyle ] [ text (toString <| run expression content) ]
+        ]
+
 
 myStyle =
-  style
-    [ ("width", "100%")
-    , ("height", "40px")
-    , ("padding", "10px 0")
-    , ("font-size", "2em")
-    , ("text-align", "center")
-    ]
+    style
+        [ ( "width", "100%" )
+        , ( "height", "40px" )
+        , ( "padding", "10px 0" )
+        , ( "font-size", "2em" )
+        , ( "text-align", "center" )
+        ]
