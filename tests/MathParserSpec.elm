94f1535cc1e5779ed959dc25193261409e6fc753
Harry Sarson <harry.sarson@hotmail.co.uk>
2018-09-07 11:46:14 +0100

    Move operator precidence tests into their own file.

--- tests/MathParserSpec.elm
+++ tests/MathParserSpec.elm
@@ -131,37 +131,4 @@ tests =
                     |> expression (always Nothing)
                     |> Result.map .expression
                     |> Expect.equal expectedAst
-        , describe "Operator precedence"
-            [ makePrecedenceTest "7 + 8"
-            , makePrecedenceTest "(aA0 - bB1) + cC2"
-            , makePrecedenceTest "(((123123 / 12314) + (12313 * 1231241)) - 123) - 1"
-            , makePrecedenceTest "(6 / 5) - 1"
-            , makePrecedenceTest "(4 * 6) + 2"
-            , makePrecedenceTest "2 - (8 / 7)"
-            , makePrecedenceTest "2 + (-6)"
-            , makePrecedenceTest "(+str) - 19"
-            , makePrecedenceTest "(8 * (-2)) - (STR / 7)"
-            , makePrecedenceTest "2 - (4 / (-8))"
-            ]
         ]
-
-
-makePrecedenceTest : String -> Test.Test
-makePrecedenceTest withParenthesis =
-    let
-        withoutParenthesis =
-            withParenthesis
-                |> String.split "( "
-                |> String.join ""
-                |> String.split " )"
-                |> String.join ""
-    in
-    test
-        (withoutParenthesis ++ " == " ++ withParenthesis)
-    <|
-        \() ->
-            withoutParenthesis
-                |> expression (always Nothing)
-                |> Result.map .expression
-                |> Result.map (MathToString.stringifyExpression <| always "DEBUG")
-                |> Expect.equal (Ok withParenthesis)
