554c8b42cbe8840bd6a153f0ae6ca5695f09a5fd
Harry Sarson <harry.sarson@hotmail.co.uk>
2018-09-07 11:16:22 +0100

    Add tests for evaluation

---
+++ tests/MathEvaluatorSpec.elm
@@ -0,0 +1,63 @@
+module MathEvaluatorSpec exposing (tests)
+
+import Expect
+import Fuzz
+import MaFuzz
+import MathEvaluator
+import MathFunction
+import MathParser
+import ParserError
+import String
+import Test exposing (describe, fuzz, fuzz2, fuzz3, test)
+
+
+tests : Test.Test
+tests =
+    describe "MathEvaluator"
+        [ describe "evaluate"
+            [ evaluatorTest "6" 6
+            , evaluatorTest "(4)" 4
+            , evaluatorTest "5 + 5" 10
+            , evaluatorTest "5 / 2" 2.5
+            , evaluatorTest "7 ^ 2 + 1" 50
+            , evaluatorTest "3 * 3 - 3 ^ 2" 0
+            , evaluatorTest "sqrt[64]" 8
+            , evaluatorTest "4 + (5 / 2)" 6.5
+            , evaluatorTest "+5" 5
+            , evaluatorTest "-6 / 2" -3
+            , evaluatorTest "4.4" 4.4
+            , evaluatorTest "123.568e12" 1.23568e14
+            ]
+        ]
+
+
+tolerance =
+    Expect.AbsoluteOrRelative 1.0e-10 1.0e-10
+
+
+evaluatorTest : String -> Float -> Test.Test
+evaluatorTest expression expected =
+    test
+        (expression ++ " ~= " ++ String.fromFloat expected)
+    <|
+        \() ->
+            expression
+                |> MathParser.expression MathFunction.fromString
+                |> (\parsedResult ->
+                        case parsedResult of
+                            Ok parsed ->
+                                parsed.expression
+                                    |> MathEvaluator.evaluate MathFunction.toRealFunction
+                                    |> (\evaluatedResult ->
+                                            case evaluatedResult of
+                                                Ok actual ->
+                                                    actual
+                                                        |> Expect.within tolerance expected
+
+                                                Err e ->
+                                                    Expect.fail (Debug.toString e)
+                                       )
+
+                            Err e ->
+                                Expect.fail (Debug.toString e)
+                   )
