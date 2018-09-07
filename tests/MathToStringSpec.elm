6ee46f10edfe276c5b8590d9dea8103ef72b2fec
Harry Sarson <harry.sarson@hotmail.co.uk>
2018-09-07 11:38:14 +0100

    Add tests for MathToString

---
+++ tests/MathToStringSpec.elm
@@ -0,0 +1,50 @@
+module MathToStringSpec exposing (tests)
+
+import Expect
+import MathEvaluator
+import MathFunction
+import MathParser
+import MathToString
+import Test exposing (describe, fuzz, fuzz2, fuzz3, test)
+
+
+tests : Test.Test
+tests =
+    describe "MathToString"
+        [ describe "evaluate"
+            [ mathToStringTest "6"
+            , mathToStringTest "5 + 5"
+            , mathToStringTest "5 / 2"
+            , mathToStringTest "( 7 ^ 2 ) + 1"
+            , mathToStringTest "( 3 * 3 ) - ( 3 ^ 2 )"
+            , mathToStringTest "sqrt[64]"
+            , mathToStringTest "4 + ( 5 / 2 )"
+            , mathToStringTest "+ 5"
+            , mathToStringTest "( - 6 ) / 2"
+            , mathToStringTest "4.4"
+            , mathToStringTest "123.568e12"
+            ]
+        ]
+
+
+mathToStringTest : String -> Test.Test
+mathToStringTest expression =
+    test expression <|
+        \() ->
+            expression
+                |> MathParser.expression MathFunction.fromString
+                |> Result.map
+                    (.expression
+                        >> MathToString.stringifyExpression MathFunction.toString
+                    )
+                |> Expect.all
+                    [ Expect.equal (Ok expression)
+                    , Result.map
+                        (MathParser.expression MathFunction.fromString
+                            >> Result.map
+                                (.expression
+                                    >> MathToString.stringifyExpression MathFunction.toString
+                                )
+                        )
+                        >> Expect.equal (Ok <| Ok expression)
+                    ]
